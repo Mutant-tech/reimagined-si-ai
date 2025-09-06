a39cbcdfc015ced2a0f6950aa1f9234331238553
Mutant-tech <jimmybeidoun@outlook.com>
2025-09-05 23:17:40 -0400

    Delete Skins.js

--- Skins.js
+++
@@ -1,7 +0,0 @@
-export const SKINS = [
-  { name: 'Blue', color: '#2b7cff' },
-  { name: 'Green', color: '#3ccf5e' },
-  { name: 'Purple', color: '#9b59ff' },
-  { name: 'Yellow', color: '#ffd24d' }
-];
-export default SKINS;
